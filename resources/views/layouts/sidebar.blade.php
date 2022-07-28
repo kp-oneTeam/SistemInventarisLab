274655ba0312bd345fb72a5fa954801c3b6b4f5a
nurulkafi <nurulkafi18@gmail.com>
2022-07-28 11:49:53 +0700

    install template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Sistem Inventaris</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">SI</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="@yield('dashboard')">
+                <a class="nav-link" href="blank.html">
+                    <i class="fas fa-columns"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="menu-header">Manajemen Barang</li>
+            <li class="@yield('vendor')"><a class="nav-link" href="{{ url('admin/vendor') }}"><i
+                        class="fas fa-tags"></i> <span>Vendor</span></a></li>
+            <li class="@yield('barang')"><a class="nav-link" href="{{ url('admin/barang') }}"><i
+                        class="fas fa-archive"></i> <span>Barang</span></a></li>
+            <li class="menu-header">Manajemen Ruangan</li>
+            <li class="@yield('inventaris')"><a class="nav-link" href="{{ url('admin/vendor') }}"><i
+                        class="fas fa-door-open"></i> <span>Ruangan</span></a></li>
+            <li class="menu-header">Manajemen Inventaris</li>
+            <li class="@yield('inventaris')"><a class="nav-link" href="{{ url('admin/vendor') }}"><i
+                        class="fas fa-boxes"></i> <span>Inventaris</span></a></li>
+            <li class="@yield('inventaris_komputer')"><a class="nav-link" href="{{ url('admin/barang') }}"><i
+                        class="fas fa-desktop"></i> <span>Inventaris Komputer</span></a></li>
+            <li class="@yield('peminjaman')"><a class="nav-link" href="{{ url('admin/barang') }}"><i
+                        class="fas fa-people-carry"></i> <span>Peminjaman</span></a></li>
+            <li class="menu-header">Laporan</li>
+            <li class="@yield('peminjaman')"><a class="nav-link" href="{{ url('admin/barang') }}"><i
+                        class="fas fa-file-pdf"></i> <span>Laporan Tahunan</span></a></li>
+        </ul>
+    </aside>
+</div>
---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/css/components.css')}}">
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+   <script src="https://cdn.ckeditor.com/4.16.1/standard/ckeditor.js"></script>
+

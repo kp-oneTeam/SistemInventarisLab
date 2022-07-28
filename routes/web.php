cc9e40897701e9809f0f7e9ada8f108ab902cce1
nurulkafi <nurulkafi18@gmail.com>
2022-07-28 13:30:44 +0700

    front end halaman login

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Route::get('/barang',[BarangController::class,'index']);

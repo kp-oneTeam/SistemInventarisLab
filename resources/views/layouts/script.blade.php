832ea66c8e8aa06fa819049a6ed1edeee692fd14
nurulkafi <nurulkafi18@gmail.com>
2022-08-01 10:25:37 +0700

    perubahan resources

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+{{-- <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -7,3 +7,31 @@
   <script src="{{ asset('admin/js/stisla.js') }}"></script>
   <script src="{{ asset('admin/js/scripts.js') }}"></script>
   <script src="{{ asset('admin/js/custom.js') }}"></script>
+<script src="{{ asset('admin/modules/select2/dist/js/select2.full.min.js')}}"></script> --}}
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('admin/modules/jquery.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/popper.js')}}"></script>
+  <script src="{{ asset('admin/modules/tooltip.js')}}"></script>
+  <script src="{{ asset('admin/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/moment.min.js')}}"></script>
+  <script src="{{ asset('admin/js/stisla.js')}}"></script>
+
+  <!-- JS Libraies -->
+  <script src="{{ asset('admin/modules/cleave-js/dist/cleave.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/cleave-js/dist/addons/cleave-phone.us.js')}}"></script>
+  <script src="{{ asset('admin/modules/jquery-pwstrength/jquery.pwstrength.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+  <script src="{{ asset('admin/modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/bootstrap-timepicker/js/bootstrap-timepicker.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/select2/dist/js/select2.full.min.js')}}"></script>
+  <script src="{{ asset('admin/modules/jquery-selectric/jquery.selectric.min.js')}}"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="{{ asset('admin/js/page/forms-advanced-forms.js')}}"></script>
+
+  <!-- Template JS File -->
+  <script src="{{ asset('admin/js/scripts.js')}}"></script>
+  <script src="{{ asset('admin/js/custom.js')}}"></script>

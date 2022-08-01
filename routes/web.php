0a67d44168589279db9adef4a337471552295fbf
nurulkafi <nurulkafi18@gmail.com>
2022-08-01 10:21:49 +0700

    inventaris dan laporan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\InventarisController;
+use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\RuanganController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +25,9 @@ Route::post('tambah/barang', [BarangController::class, 'tambah_barang']);
 Route::delete('hapus/barang/{id}', [BarangController::class, 'hapus_barang']);
 Route::get('detail/barang/{id}', [BarangController::class, 'detail_barang']);
 Route::get('/ruangan',[RuanganController::class,'index']);
+Route::get('/inventaris', [InventarisController::class, 'index']);
+Route::get('tambah/inventaris', [InventarisController::class, 'form_tambah_inventaris']);
+Route::post('tambah/inventaris', [InventarisController::class, 'tambah_inventaris']);
+Route::get('edit/inventaris/{id}', [InventarisController::class, 'form_ubah_inventaris']);
+Route::delete('hapus/inventaris/{id}', [InventarisController::class, 'hapus_inventaris']);
+Route::get('laporan',[LaporanController::class,'index']);

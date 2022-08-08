1bd334cc4d2d6b5449571e4133895190915df5aa
Doni Hadimas <donihadimasaprilian@gmail.com>
2022-08-08 11:28:02 +0700

    Penambahan Fitur Tandai semua untuk hapus data

---
+++ database/migrations/2022_08_08_034621_peminjaman.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->string('kodePeminjaman');
+            $table->string('noIdentitas');
+            $table->string('namaPeminjam');
+            $table->string('tujuanPeminjaman');
+            $table->date('tglPeminjaman');
+            $table->date('tglKembali');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
